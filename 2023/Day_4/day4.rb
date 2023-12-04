c58320ce1e7261c0c7db7c2e02496a61d9a7bc5d
Mathieu Portmann <mathieu.rails@gmail.com>
2023-12-04 19:43:51 +0100

    jour 4 2023

---
+++ 2023/Day_4/day4.rb
@@ -0,0 +1,34 @@
+resultat1 = 0
+resultat2 = 0
+
+# id_card => nb copy
+card_owned = Hash.new(0)
+
+File
+  .open("data")
+  .readlines
+  .each do |line|
+    cards, numbers = line.split(":")
+    id_card = cards.split(" ")[1].to_i
+    main, winning = numbers.split("|")
+    main = main.split(/\s+/).reject(&:empty?)
+    winning = winning.split(/\s+/).reject(&:empty?)
+
+    res = main & winning
+    if !res.empty?
+      if res.length > 1
+        resultat1 += (2**(res.length - 1))
+      else
+        resultat1 += 1
+      end
+    end
+
+    card_owned[id_card] += 1 || 1
+
+    ((id_card + 1)...(id_card + res.length + 1)).each do |nb|
+      card_owned[nb] += card_owned[id_card]
+    end
+  end
+
+puts resultat1
+puts card_owned.values.reduce(:+)

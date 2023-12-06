5c6f278b65949f183f7e25829ec3fda703c390bb
Mathieu Portmann <mathieu.rails@gmail.com>
2023-12-06 19:10:04 +0100

    jour 6 2023 - opti

--- 2023/Day_6/day6.rb
+++ 2023/Day_6/day6.rb
@@ -1,3 +1,5 @@
+require "benchmark"
+
 time_distance = Array.new
 File
   .open("data")
@@ -17,5 +19,42 @@ def calc_race_win(time_distance)
   res
 end
 
+# Solution 1
 puts calc_race_win(time_distance).reduce(&:*)
-puts calc_race_win([[60_947_882], [475_213_810_151_650]]).reduce(&:*)
+
+time = time_distance[0].join("").to_i
+dist = time_distance[1].join("").to_i
+
+# Solution 2 - non opti
+Benchmark.bm(20) do |bm|
+  bm.report("Solution 2 - non opti:") do
+    calc_race_win([[time], [dist]]).reduce(&:*)
+  end
+
+  # Solution 2 - opti
+  bm.report("Solution 2 - opti:") do
+    content = File.open("data").readlines
+
+    x = 0
+    (0..time).each do |t|
+      if t * (time - t) > dist
+        x = t
+        break
+      end
+    end
+
+    y = 0
+    (time).downto(0) do |t|
+      if t * (time - t) > dist
+        y = t
+        break
+      end
+    end
+  end
+end
+
+=begin
+                           user     system      total        real
+Solution 2 - non opti:  3.995637   0.001175   3.996812 (  4.015734)
+Solution 2 - opti:     0.793084   0.000125   0.793209 (  0.794136)
+=end
